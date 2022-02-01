77cfbc7c45a9c0c8bc758aacde73fdfe0d7a2a0e
LucaTedeschini <luca.tedeschini00@gmail.com>
2022-02-01 18:32:17 +0100

    update webcrawler and Documenti.tar

--- webcrawler/metacritics.py
+++ webcrawler/metacritics.py
@@ -1,3 +1,4 @@
+from distutils import text_file
 import bs4, requests, webbrowser
 from matplotlib.pyplot import title
 import functools
@@ -7,6 +8,11 @@ import os
 from pprint import pprint
 import urllib.parse
 import time
+
+
+PATH_TO_DIRECTORY = "../../DocumentiTest" #Teddy
+#I file sono 18828 (contati con wc, non sono calcolati dinamicamente, magari TODO di aggiungere il calcolo dinamico)
+FILE_NUMBER = 18828
 
 def getMark(game_name):
     
@@ -21,17 +27,31 @@ def getMark(game_name):
         results = parser.find('ul', class_='search_results module').find_all('a', href = True)
         listed_games = [result["href"] for result in results]
 
-        #print(f"\nHai scelto : {listed_games[0]}")
+        try:
+            if len(listed_games) > 1:
+                i = 1
+                for result in listed_games:
+                    print(f"{i}. {result}")
+                    i += 1
+                print("x. Il gioco non è presente")
+                choice = input("Choose the right number: ")
+                if choice == 'x':
+                    raise Exception("Gioco non trovato")
+            else:
+                choice = 1
+            gamepage_link = domain+listed_games[int(choice)-1]
+            #print(f"\nHai scelto : {listed_games[0]}")
 
-        gamepage_link = domain+listed_games[0]
-        UserAgent = Request(gamepage_link, headers={ 'User-Agent' : 'Mozilla/5.0' })
-        html = urlopen(UserAgent)
-        mark = BeautifulSoup(html, 'html.parser').find('span', itemprop='ratingValue').get_text()
-        
-        if mark.isnumeric():
-            return mark
-        else:
-            raise TypeError("N\A")
+            UserAgent = Request(gamepage_link, headers={ 'User-Agent' : 'Mozilla/5.0' })
+            html = urlopen(UserAgent)
+            mark = BeautifulSoup(html, 'html.parser').find('span', itemprop='ratingValue').get_text()
+            
+            if mark.isnumeric():
+                return mark
+            else:
+                raise TypeError("N\A")
+        except:
+            raise Exception("Gioco non trovato")
     except:
         raise Exception("Gioco non trovato")
 
@@ -39,17 +59,28 @@ def getMark(game_name):
 
 import os
 #E' dove ho io la lista di documenti... Al momento lascio così perché a github non gli piacciono i 18000 file e quindi li tengo fuori dal progetto
-directory = '../../Documenti'
+directory = PATH_TO_DIRECTORY
  
 
+c=0
 for filename in os.listdir(directory):
+    c+=1
+    print(f"####################################\n{c}/{FILE_NUMBER}\n####################################")
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
         #time.sleep(0.5)
         file_game = open(f,'r')
         game_name = file_game.readline()
-        title = game_name[8:-1]
-        try:
-            print(title+": "+getMark(title))
-        except:
-            print(f"{title}: Gioco non trovato")
+        text_from_file = file_game.read()
+        if (text_from_file.find("Valutazione:") < 0):
+            title = game_name[8:-1]
+            print(f"Ricerco {game_name}")
+            file_game.close()
+            file_game = open(f,'a')
+            try:
+                voto = getMark(title)
+                print(title+": "+str(voto))
+                file_game.write("\n\nValutazione: "+str(voto)+"\n")
+            except:
+                print(f"{title}: Gioco non trovato")
+                file_game.write("\n\nValutazione: Non presente\n")
