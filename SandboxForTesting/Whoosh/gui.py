32a9a04b3765ebecfa36a1f828312adec89c9556
ChristianP01 <christianpreti01@gmail.com>
2022-04-05 13:23:35 +0200

    Minor fixes

--- SandboxForTesting/Whoosh/gui.py
+++ SandboxForTesting/Whoosh/gui.py
@@ -13,7 +13,6 @@ class Logic(baseUIWidget, baseUIClass):
     def __init__(self, parent=None):
         super(Logic, self).__init__(parent)
         self.setupUi(self)
-        #self.resultsText.setPlainText( main.searchQuery(self, str( self.userQuery.toPlainText() )) )
         self.searchButton.clicked.connect(lambda: main.searchQuery(self, str(self.userQuery.toPlainText())) )
 
 if __name__ == "__main__":
--- SandboxForTesting/Whoosh/main.py
+++ SandboxForTesting/Whoosh/main.py
@@ -1,6 +1,7 @@
 '''
 Reference: https://whoosh.readthedocs.io/en/latest/quickstart.html
 '''
+from pprint import pprint
 from whoosh.fields import Schema, TEXT
 from whoosh.query import *
 from whoosh.index import open_dir
@@ -32,13 +33,31 @@ def readDocument(path):
         raise FileNotFoundError
 
 
+def readDocument2(path):
+    '''
+    Funzione che legge il documento passato come path.
+    Il documento deve essere correttamente formattato.
+    Returna una tupla del tipo (titolo, descrizione, valutazione, genere)
+    '''
+    try:
+        with open(path, "r") as file:
+            content = file.read()
+            title : str = content[content.find("Titolo:") + len("Titolo: ") : content.find("\n", content.find("Titolo:"))]
+            description : str = content[content.find("Descrizione:") + len("Descrizione: ") : content.find("\n", content.find("Descrizione:"))]
+            mark : str = content[content.find("Valutazione:") + len("Valutazione: ") : content.find("\n", content.find("Valutazione:"))]
+            genres : str = content[content.find("Genere:") + len("Genere: ") : content.find("\n", content.find("Genere:"))]
+            return (title, description, mark, genres)
+    except:
+        raise FileNotFoundError
+
+
 if __name__ == "__main__":
     pass
 
 def openIndex():
     #Sto copiando dal tutorial, ma direi di star creando appunto uno schema che ha
     #un titolo in formato testuale e un contenuto in formato testuale
-    schema = Schema(title=TEXT(stored=True), content=TEXT)
+    schema = Schema(title=TEXT(stored=True), content=TEXT(stored=True), mark=TEXT(stored=True), genres=TEXT(stored=True) )
 
     #Leggo dalla guida che si possono creare i propri tipi di fields
 
@@ -55,26 +74,28 @@ def openIndex():
         from whoosh.index import open_dir
 
         ix = open_dir("index")
-
-
-
         writer = ix.writer()
-
+        genres_list = []
 
         from os import listdir
 
         for f in listdir(PATH_TO_DOCUMENT):
-            document = readDocument(PATH_TO_DOCUMENT+"/"+f)
+            document = readDocument2(PATH_TO_DOCUMENT+"/"+f)
             #Writer accetta stringhe in unicode, ma ho letto che tutte le stringhe in python3 sono in unicode quindi polleg
-            writer.add_document(title=document[0], content=document[1])
+            writer.add_document(title=document[0], content=document[1], mark = document[2], genres = document[3])
+
+            if document[3] not in genres_list:
+                genres_list.append(document[3])
+
+
+        print(f"Tutti i generi trovati sono:")
+        pprint(genres_list)
         
         #Salva i documenti nell'indice
         writer.commit()
     else:
         ix = index.open_dir("index")
     return ix
-
-
 
 def searchQueryCLI(user_query):
     ix = openIndex()
@@ -91,23 +112,20 @@ def searchQueryCLI(user_query):
         print(results)
         user_query = yield results
 
-
 def searchQuery(gui, user_query):
+
     ix = openIndex()
     #Creo oggetto searcher
     with ix.searcher() as searcher:
-        #Provo a fare una versione che prenda un numero indefinito di parametri
         word_list = user_query.split(" ")
         print(f"Lista parole: {word_list}")
         
         L = []
         for word in word_list:
             L.append(Term("content", word))
-        query = And("content", L)
 
+        query = And(L)
         results = searcher.search(query)
-        
-
         gui.resultsText.setPlainText("") #Inizializzo il valore iniziale del box risultati
 
         for r in results: #Appende i vari risultati singoli all'interno della lista
