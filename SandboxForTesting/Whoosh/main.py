1855223405d175b0ac7480fe9173da2de4dd7f24
LucaTedeschini <luca.tedeschini00@gmail.com>
2022-05-07 11:36:58 +0200

    test branching

--- SandboxForTesting/Whoosh/main.py
+++ SandboxForTesting/Whoosh/main.py
@@ -215,7 +215,7 @@ def proximitySearch(word_list, ix):
 def searchByMark(word_list, ix, mark_min=None, mark_max=None):
 
     marked_games = []
-
+    #FATTO DA ME LUCA TEDESCHINI QUESTA RIGA E' MIA
     if mark_min is None:
         mark_min = 0
 
