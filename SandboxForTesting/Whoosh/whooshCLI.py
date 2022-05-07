b13be3cd55ef69536d10cd30fd74d59b0c984977
LucaTedeschini <luca.tedeschini00@gmail.com>
2022-05-07 11:33:48 +0200

    joined search

--- SandboxForTesting/Whoosh/whooshCLI.py
+++ SandboxForTesting/Whoosh/whooshCLI.py
@@ -7,8 +7,21 @@ qgen.send(None)
 while True:
     q = input("Inserisci ricerca: ")
 
-
+    print("\n\n\n\n\n\n")
     result = qgen.send(q)
-    print(result)
-    for r in result:
+    #print(result)
+    '''
+    print("RICERCA PER TITOLO")
+    for r in result[0]:
         print(f"{r['title']} con rank {r.score}")
+
+    print("RICERCA PER DESCRIZIONE")
+    for r in result[1]:
+        print(f"{r['title']} con rank {r.score}")
+    '''
+
+    print("RICERCA JOINATA TITLE E DESCRIPTION")
+
+    sorted_result = sorted(result.items(), key=lambda x: x[1], reverse=True)
+    for res in sorted_result[:10]:
+        print(f"{res[0]} con valutazione {res[1]}")
