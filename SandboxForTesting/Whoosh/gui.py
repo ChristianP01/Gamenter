53d3647ff7b227046c74b8a66e3d93cbda8fcd46
ChristianP01 <christianpreti01@gmail.com>
2022-05-10 14:48:36 +0200

    Filled ComboBox with genres, frontend

--- SandboxForTesting/Whoosh/gui.py
+++ SandboxForTesting/Whoosh/gui.py
@@ -5,7 +5,7 @@ from whoosh.fields import Schema, TEXT
 import sys
 import os.path
 from whoosh.index import create_in
-import main
+import searcher
 
 baseUIClass, baseUIWidget = uic.loadUiType("gui.ui")
 
@@ -13,7 +13,16 @@ class Logic(baseUIWidget, baseUIClass):
     def __init__(self, parent=None):
         super(Logic, self).__init__(parent)
         self.setupUi(self)
-        self.searchButton.clicked.connect(lambda: main.searchQuery(self, str(self.userQuery.toPlainText())) )
+        self.genreList.setPlaceholderText(" ")
+        f = open("generi_meglio.txt", "r")
+        genres = f.readlines()
+
+        for genre in genres:
+            self.genreList.addItem(genre)
+
+        f.close()
+
+        self.searchButton.clicked.connect(lambda: searcher.searchQuery(self, str(self.userQuery.toPlainText())) )
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
